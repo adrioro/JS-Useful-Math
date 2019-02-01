4ed32772daf2e8a9e8d0ba403fb7b125697c7ad6
adrioro <artistsvillage@me.com>
2019-02-01 16:18:56 -0500

    Third commit

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+//Math.round();
+
 var roundThis = 3.58409;
 $("#button").click(function () {
     var newNum = Math.round(roundThis);
     $(this).after(" <br><br>" + newNum + "<br> <br>");
-})
+});
+
+//Math.pow();
+var powerThis = Math.pow(2, 5);
+$("#power").click(function () {
+    var newNum = Math.pow(roundThis);
+    $(this).after(" <br><br>" + powerThis + "<br> <br>");
+});
+//PI is an object, not a method
+
+var p = Math.PI; //p for Pi = 3.141592653589793...
+$("#pi").click(function () {
+    $('#demo').text("PI is: " + p); // Addintg text to existing <p> with id=demo.
+});
